bd5fe682c798e15c0050d0ecf258b7945db690ed
xnestea <juszczak.kajetan@gmail.com>
2018-12-06 16:32:11 +0100

    6.12_commit_1

--- DESeq2.R
+++ DESeq2.R
@@ -13,16 +13,29 @@ setwd("project_HT/Data/")
 counts <- as.matrix(read.csv(file = "raw/genes.raw.htseq2.tsv",sep = "\t", row.names = 1))
 
 ### read in metadata file and restrict it to data we are interested in
+### also drop technical duplicates since there are not present in count matrix
 metadata <- read.csv("E-MTAB-2328.sdrf.txt", sep = "\t")
 metadata <- subset(metadata, Characteristics.developmental.stage. %in% c("embryonic day 15.5", "postnatal day 29"))
 metadata <- subset(metadata, Characteristics.organism.part. %in% c("liver"))
-
+metadata <- metadata[!duplicated(metadata$Assay.Name),]
 ### get sample names we are interested in from restricted metadata
 ### and restrict our read count matrix t only have samples we are interested in
 list_of_names <- metadata$Scan.Name
 list_of_names <- substr(list_of_names, start = 1, stop = 6)
-counts[, colnames = colnames(counts) %in% c(list_of_names)]
+counts <- counts[, colnames = colnames(counts) %in% c(list_of_names)]
 
 #to be continued
-#metadata <- metadata[,duplicated(metadata$Source.Name)]
-#deseq <- DESeqDataSetFromMatrix(countData=counts, colData=metadata, design= ~Characteristics.developmental.stage.)
+
+### perform DESeq analysis
+deseq <- DESeqDataSetFromMatrix(countData=counts, colData=metadata, design= ~Characteristics.developmental.stage.)
+analysis <- DESeq(deseq)
+### visualizing results to be clearly visible.
+res = results(analysis, 
+              contrast=c("Characteristics.developmental.stage.","embryonic day 15.5","postnatal day 29"), 
+              lfcThreshold = 0,altHypothesis="greaterAbs",
+              alpha = 0.01)
+
+write.table(res ,file = "DESEQ2_out.txt", sep = "\t",row.names = T,col.names = NA)
+### PIANO PART
+
+
